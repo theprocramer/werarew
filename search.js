33efc621baf901c94c819165657d1b7717bfa3d5
theprocramer <theprocramer@gmail.com>
2025-03-02 01:28:31 +0400

    Update search.js

--- search.js
+++ search.js
@@ -33,35 +33,4 @@
         name: "sheet-werarew sheet",
         link: "https://theprocramer.github.io/werarew-sheet",
         description: "sheet"
-      }
-    ];
-
-    function displayResults() {
-      const query = document.getElementById('searchBar').value.toLowerCase();
-      const resultsList = document.getElementById('searchResultsList');
-      resultsList.innerHTML = ''; // Önceki sonuçları temizle
-
-      const filteredData = staticData.filter(item => item.name.toLowerCase().includes(query));
-
-      if (filteredData.length > 0) {
-        filteredData.forEach(result => {
-          const li = document.createElement('li');
-          li.classList.add('list-item');
-          li.innerHTML = `
-            <a href="${result.link}" target="_blank">${result.name}</a>
-            <p>${result.description}</p>
-          `;
-          resultsList.appendChild(li);
-        });
-      } else {
-        const li = document.createElement('li');
-        li.classList.add('list-item');
-        li.textContent = 'Arama sonuçları bulunamadı.';
-        resultsList.appendChild(li);
-      }
-    }
-
-    // Arama yapılırken sonuçları listele
-    function searchResults() {
-      displayResults();
-    }
+     
